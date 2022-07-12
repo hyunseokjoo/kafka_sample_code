68904e24043fc4e08530bf5da2c6692ebc3162d9
Eddie <49854618+hyunseokjoo@users.noreply.github.com>
2022-07-12 22:10:37 +0900

    add : producer sample 추가

---
+++ kafka-python/producer.py
@@ -0,0 +1,7 @@
+from ensurepip import bootstrap
+from kafka import KafkaProducer 
+
+producer = KafkaProducer(bootstrap_servers = ['localhost;9092'])
+
+producer.send('first-topic', b'hello world from python')
+producer.flush()
