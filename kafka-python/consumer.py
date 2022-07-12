fd2a0eea2315f9ff26027f3a54d5fd7ed290c82e
Eddie <49854618+hyunseokjoo@users.noreply.github.com>
2022-07-12 22:16:17 +0900

    add : consumer sample 추가

---
+++ kafka-python/consumer.py
@@ -0,0 +1,9 @@
+from ensurepip import bootstrap
+from kafka import KafkaConsumer
+
+# KafkaConsumer(토픽 이름, bootstrap_servers = broker_server주소)
+consumer = KafkaConsumer('first-topic', bootstrap_servers = ['localhost;9092'])
+
+# 아래와 같이 작성하면 Consumer로 계속 들어오기 때문에 Loop가 돌게 된다.
+for messages in consumer:
+    print(messages)
