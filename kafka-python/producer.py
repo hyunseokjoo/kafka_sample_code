701a17e4c8da70ea53615ffdf39c8ef9db93e53a
Eddie <49854618+hyunseokjoo@users.noreply.github.com>
2022-07-17 09:25:54 +0900

    fix : host 주소 수정

--- kafka-python/producer.py
+++ kafka-python/producer.py
@@ -2,7 +2,7 @@ from ensurepip import bootstrap
 from kafka import KafkaProducer 
 
 # producer 생성 
-producer = KafkaProducer(bootstrap_servers = ['localhost;9092'])
+producer = KafkaProducer(bootstrap_servers = ['localhost:9092'])
 # 메세지 보내기 message는 byte형식으로 보내는 것을 익숙해져야한다.
 producer.send('first-topic', b'hello world from python')
 # producer제거 
