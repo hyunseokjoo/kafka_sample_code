a445da615060d6d3b52c596528ae9eae3f954977
DESKTOP-EINE1B2\주현석 <joohyunseok0313@gmail.com>
2022-07-28 16:02:20 +0900

    add : kafka-to-sparkStream 정보 추가

---
+++ kafka-scala/README.md
@@ -0,0 +1,35 @@
+# Kafka-to-spark 예제 -> kafkaStreming.scala 소스 확인
+### 먼저 해야하는 설치 사항 
+hadoop version 확인
+```bash
+hadoop version
+```
+spark version 확인 
+```
+spark-submit --version
+# scala 버전이 나옴 ex) 2.12.10
+```
+scala version 확인
+```bash
+scala
+# spark와 맞는 버전 다운로드 
+```
+### docker로 kafka 올리기 
+```bash 
+cd kafka-scala
+docker-compose up -d
+```
+### topic 생성 
+```bash 
+docker exec -it kafka2 kafka-topics --bootstrap-server broker:9092 --create --topic order --replication-factor 1 --partitions 3
+```
+### producer message 개시
+```bash 
+python producer-order.py
+```
+### spark으로 message 받기 
+```
+# kafkaStreaming intellij에서 실행 하거나 jar파일 spark-submit
+
+```
+
---
+++ kafka-scala/src/main/scala/KafkaStreaming.scala
@@ -0,0 +1,54 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{SparkSession}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.streaming.Trigger
+import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
+
+object KafkaStreaming extends App {
+  // sparkSession-conf 생성
+  val conf = new SparkConf()
+    .setAppName("Spark-Kafka")
+    .set("spark.streaming.stopGracefullyOnShutdown", "true")
+
+  // session 실행
+  val spark = SparkSession.builder()
+    .master("local")
+    .config(conf)
+    .getOrCreate()
+
+  // kafka 정보 받아오기
+  val df = spark.readStream
+    .format("kafka") // format kafka
+    .option("kafka.bootstrap.servers", "localhost:9091,localhost:9092,localhost:9093") // bootstrap server 설정 여러 개일 때 ','로 구분
+    .option("subscribe", "order") 
+    .option("startingOffsets", "earliest")
+    //.option("endOffsets", "latest")
+    //.option("value.deserializer", "StringDeserializer")
+    .load()
+
+  // key, value, topic등 내용이 들어오게 되고 
+  // value에 메세지가 내용이 들어 오게 됨
+  val personStringDF = df.selectExpr("CAST(value AS STRING)")
+
+  // schema 구성 데이터의 맞게
+  val schema = new StructType()
+    .add("DATE",StringType)
+    .add("TIME",StringType)
+    .add("USER_ID",StringType)
+    .add("CUSTOMER_TYPE",StringType)
+    .add("MENU",StringType)
+    .add("PRICE",IntegerType)
+
+  // value를 json으로 변형 및 모든 데이터 select
+  val personDF = personStringDF.select(from_json(col("value"), schema).as("data"))
+    .select("data.*")
+
+  // 받아온 데이터 console로 출력
+  println(personDF)
+  personDF.writeStream
+    .format("console")
+    .outputMode("append")
+    .trigger(Trigger.ProcessingTime("20 seconds"))
+    .start()
+    .awaitTermination()
+}
